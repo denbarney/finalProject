91363a3d4651dfdd9600d2146085ec7de4244708
denbarney <denbarney@gmail.com>
2016-12-18 16:11:00 -0500

    tried to pull object info into divs on the page will work on that tonight. also created data.js.

--- javascript/main.js
+++ javascript/main.js
@@ -39,18 +39,30 @@ function getPlacesByType(type, radius=500) {
 	const typeparameter = "type="+type +"&";
 	const key = "key=AIzaSyCd-hQBzbOUihAVIpPzyKWYnbPagdj4jmE"
 	let link = rooturl + location + radiusparameter + typeparameter + key
-	console.log(link)
-	data = $.get (link)
-
+	//get data
+	return data
 }
 
 $('.typebtn').click(function(e) {
 	console.log($(this)[0].id);
 	const type=($(this)[0].id)
+	console.log(data);
 
-	getPlacesByType(type)
-	
+	/*var places = getPlacesByType(type)*/
+	let places = data.results
+
+
+	places.forEach(function(place) {
+		let el = makeCard(place)
+	    console.log(el, place.name);
+		$('#places').append(el)
+	})
 })
+
+function makeCard(place){
+	return '<div><p><strong>'+place.name+'</strong><em>'+place.rating+'</em><image width=30 src="data:image/png;base64,'+place.photos[0].photo_reference+'"/></div>'
+}
+
 
 
 // draw places
