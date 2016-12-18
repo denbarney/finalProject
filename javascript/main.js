877e914c626aff82d401ad240adf210602a49f62
denbarney <denbarney@gmail.com>
2016-12-18 14:26:52 -0500

    Sam helped me figure out how to do a link function for the places api!

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,10 @@ $('.js-main-menu').click(function() {
 		.sidebar('setting', 'dimPage', false)
 		.sidebar('toggle');
 })
+	const myLatLng = {
+		lat: 40.8200471, 
+		lng: -73.9514611
+	};
 
 function initMap() {
 
@@ -14,10 +18,7 @@ function initMap() {
 	});
 
 	
-	const myLatLng = {
-		lat: 40.8200471, 
-		lng: -73.9514611
-	};
+
 	const map = new google.maps.Map(document.getElementById('map'), {
 	  center: myLatLng,
 	  zoom: 16
@@ -29,3 +30,28 @@ function initMap() {
 		title: 'Hello World!'
 	});
 }
+
+getPlacesByType("restaurant", 500)
+function getPlacesByType(type, radius) {
+	const rooturl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+	const location = "location="+ myLatLng.lat+","+myLatLng.lng +"&";
+	const radiusparameter = "radius="+radius +"&";
+	const typeparameter = "type="+type +"&";
+	const key = "key=AIzaSyCd-hQBzbOUihAVIpPzyKWYnbPagdj4jmE"
+	let link = rooturl + location + radiusparameter + typeparameter + key
+	console.log(link)
+
+
+
+
+
+
+}
+/* get all places by type */
+
+// draw places
+
+// search places 
+
+// load retrieved content in container 
+
