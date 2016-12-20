b713d78c54724108ae6c76c438ffcb2ceb03707b
denbarney <denbarney@gmail.com>
2016-12-19 20:09:14 -0500

    Began to style my data, styled my map, put JSON on (gotta find out what that is).

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,10 @@
 
-$('.js-main-menu').click(function() {
-	$('.ui.sidebar')
-		// .sidebar('setting', 'transition', 'overlay')
-		.sidebar('setting', 'dimPage', false)
-		.sidebar('toggle');
-})
+// $('.js-main-menu').click(function() {
+// 	$('.ui.sidebar')
+// 		// .sidebar('setting', 'transition', 'overlay')
+// 		.sidebar('setting', 'dimPage', false)
+// 		.sidebar('toggle');
+// })
 	const myLatLng = {
 		lat: 40.8200471, 
 		lng: -73.9514611
@@ -17,19 +17,246 @@ function initMap() {
 		height: height + 'px',
 	});
 
+
+
 	
 
 	const map = new google.maps.Map(document.getElementById('map'), {
 	  center: myLatLng,
-	  zoom: 16
-	});
+	  zoom: 15,
+	  gestureHandling: 'auto',
+	     styles: [{
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ebe3cd"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#523735"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f1e6"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#c9b2a6"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#dcd2be"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#ae9e90"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#93817c"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#a5b076"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#447530"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f1e6"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#fdfcf8"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f8c967"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#e9bc62"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e98d58"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#db8555"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#806b63"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8f7d77"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#ebe3cd"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dfd2ae"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#b9d3c2"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#92998d"
+      }
+    ]
+  }
+]
+
+        });
 
 	const marker = new google.maps.Marker({
-		position: myLatLng,
+		position: myLatLng, 
 		map: map,
 		title: 'Hello World!'
 	});
+
+
+map.data.loadGeoJson(
+      'https://storage.googleapis.com/mapsdevsite/json/google.json');
 }
+
+
+
+
+
 /* get all places by type */
 getPlacesByType("restaurant", 500)
 function getPlacesByType(type, radius=500) {
@@ -58,11 +285,52 @@ $('.typebtn').click(function(e) {
 		$('#places').append(el)
 	})
 })
-
+// STYLING FOR DATA
 function makeCard(place){
-	return '<div><p><strong>'+place.name+'</strong><em>'+place.rating+'</em><image width=30 src="data:image/png;base64,'+place.photos[0].photo_reference+'"/></div>'
+	return `
+<div>
+	<p>
+		<strong>${place.name}</strong>
+		<em class="red">${place.rating}</em>
+		<image width="30" src="data:image/png;base64,${place.photos[0].photo_reference}"/>
+	</p>
+</div>`;
 }
 
+`
+<div class="ui items">
+	<div class="item">
+		<div class="ui small image">
+			<img src=src="data:image/png;base64,${place.photos[0].photo_reference}"/>
+		</div>
+		<div class="content">
+			<div class="header">${place.name}</div>
+
+
+
+	<p>
+		<strong>${place.name}</strong>
+		<em class="red">${place.rating}</em>
+		<image width="30" src="data:image/png;base64,${place.photos[0].photo_reference}"/>
+	</p>
+</div>`;
+
+// // <div class="ui items">
+//   <div class="item">
+//     <div class="ui small image">
+//       <img src="/images/wireframe/image.png">
+//     </div>
+//     <div class="content">
+//       <div class="header">Arrowhead Valley Camp</div>
+//       <div class="meta">
+//         <span class="price">$1200</span>
+//         <span class="stay">1 Month</span>
+//       </div>
+//       <div class="description">
+//         <p></p>
+//       </div>
+//     </div>
+//   </div>
 
 
 // draw places
@@ -70,4 +338,5 @@ function makeCard(place){
 // search places 
 
 // load retrieved content in container 
+
 
